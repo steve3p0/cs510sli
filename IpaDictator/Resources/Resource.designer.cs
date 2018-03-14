a60ef18e7131d49d0a3eeec87fc1187cf1c54292
steve <stevebpdx@gmail.com>
2018-03-14 07:26:23 -0700

    Compiles!!!!

--- IpaDictator/Resources/Resource.designer.cs
+++ IpaDictator/Resources/Resource.designer.cs
@@ -82,8 +82,8 @@ namespace IpaDictator
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060004
-			public const int btnRecord = 2131099652;
+			// aapt resource value: 0x7f060005
+			public const int btnRecord = 2131099653;
 			
 			// aapt resource value: 0x7f060000
 			public const int imageButton1 = 2131099648;
@@ -91,20 +91,23 @@ namespace IpaDictator
 			// aapt resource value: 0x7f060001
 			public const int linearLayout1 = 2131099649;
 			
-			// aapt resource value: 0x7f060005
-			public const int linearLayout2 = 2131099653;
+			// aapt resource value: 0x7f060006
+			public const int linearLayout2 = 2131099654;
 			
-			// aapt resource value: 0x7f060007
-			public const int linearLayout3 = 2131099655;
+			// aapt resource value: 0x7f060008
+			public const int linearLayout3 = 2131099656;
+			
+			// aapt resource value: 0x7f060003
+			public const int space1 = 2131099651;
 			
 			// aapt resource value: 0x7f060002
 			public const int textView1 = 2131099650;
 			
-			// aapt resource value: 0x7f060003
-			public const int textView2 = 2131099651;
+			// aapt resource value: 0x7f060004
+			public const int textView2 = 2131099652;
 			
-			// aapt resource value: 0x7f060006
-			public const int textYourText = 2131099654;
+			// aapt resource value: 0x7f060007
+			public const int textYourText = 2131099655;
 			
 			static Id()
 			{
