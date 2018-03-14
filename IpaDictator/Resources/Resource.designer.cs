3c617981e072b7f13003bc24443e30214737713c
steve <stevebpdx@gmail.com>
2018-03-14 09:48:12 -0700

    Version 1

--- IpaDictator/Resources/Resource.designer.cs
+++ IpaDictator/Resources/Resource.designer.cs
@@ -61,13 +61,19 @@ namespace IpaDictator
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int icon = 2130837504;
+			public const int clipboard = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int microphone_blue = 2130837505;
+			public const int clipboard3 = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int textViewBorder = 2130837506;
+			public const int icon = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int microphone_blue = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int textViewBorder = 2130837508;
 			
 			static Drawable()
 			{
